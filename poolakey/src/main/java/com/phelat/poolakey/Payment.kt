5aa059948ad4ab8d992ac2d057fda3dabef49a8d
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-09-28 00:44:33 +0330

    Implement a simple form of poolakey's 'initialize' function
    We need 'initialize' function to bind to Bazaar's billing service

---
+++ poolakey/src/main/java/com/phelat/poolakey/Payment.kt
@@ -0,0 +1,31 @@
+package com.phelat.poolakey
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+
+class Payment : ServiceConnection {
+
+    fun initialize(context: Context) {
+        Intent(BILLING_SERVICE_ACTION).apply { `package` = BAZAAR_PACKAGE_NAME }
+            .takeIf { context.packageManager.queryIntentServices(it, 0).isNotEmpty() }
+            ?.also { context.bindService(it, this, Context.BIND_AUTO_CREATE) }
+            ?: run {
+                TODO("Service is not available")
+            }
+    }
+
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {
+    }
+
+    companion object {
+        private const val BILLING_SERVICE_ACTION = "ir.cafebazaar.pardakht.InAppBillingService.BIND"
+        private const val BAZAAR_PACKAGE_NAME = "com.farsitel.bazaar"
+    }
+
+}
