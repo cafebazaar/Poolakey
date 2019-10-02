c33dfb705c8719ed4293e0561b65c5d134e95182
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-10-03 01:42:54 +0330

    Return ConnectionCallback from 'initialize' function

--- poolakey/src/main/java/com/phelat/poolakey/BillingConnection.kt
+++ poolakey/src/main/java/com/phelat/poolakey/BillingConnection.kt
@@ -13,12 +13,15 @@ internal class BillingConnection(private val context: Context) : ServiceConnecti
 
     private var billingService: IInAppBillingService? = null
 
-    internal fun startConnection(connectionCallback: ConnectionCallback.() -> Unit) {
+    internal fun startConnection(
+        connectionCallback: ConnectionCallback.() -> Unit
+    ): ConnectionCallback {
         callback = ConnectionCallback().apply(connectionCallback)
         Intent(BILLING_SERVICE_ACTION).apply { `package` = BAZAAR_PACKAGE_NAME }
             .takeIf { context.packageManager.queryIntentServices(it, 0).isNotEmpty() }
             ?.also { context.bindService(it, this, Context.BIND_AUTO_CREATE) }
             ?: run { callback?.connectionFailed?.invoke() }
+        return requireNotNull(callback)
     }
 
     override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
--- poolakey/src/main/java/com/phelat/poolakey/Payment.kt
+++ poolakey/src/main/java/com/phelat/poolakey/Payment.kt
@@ -6,8 +6,8 @@ class Payment(context: Context) {
 
     private val connection = BillingConnection(context)
 
-    fun initialize(callback: ConnectionCallback.() -> Unit) {
-        connection.startConnection(callback)
+    fun initialize(callback: ConnectionCallback.() -> Unit): ConnectionCallback {
+        return connection.startConnection(callback)
     }
 
 }
