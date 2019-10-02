db5edc71f0815b792fcb524ff05315f883e635b5
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-10-03 00:32:42 +0330

    Encapsulate 'billingService' and ServiceConnection to an internal class
    Applying this will give developer a more stable experience since he/she
    can't have access to the internal service connection.

---
+++ poolakey/src/main/java/com/phelat/poolakey/BillingConnection.kt
@@ -0,0 +1,53 @@
+package com.phelat.poolakey
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import com.android.vending.billing.IInAppBillingService
+
+internal class BillingConnection(private val context: Context) : ServiceConnection {
+
+    private var billingService: IInAppBillingService? = null
+
+    internal fun startConnection() {
+        Intent(BILLING_SERVICE_ACTION).apply { `package` = BAZAAR_PACKAGE_NAME }
+            .takeIf { context.packageManager.queryIntentServices(it, 0).isNotEmpty() }
+            ?.also { context.bindService(it, this, Context.BIND_AUTO_CREATE) }
+            ?: run {
+                TODO("Service is not available")
+            }
+    }
+
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        IInAppBillingService.Stub.asInterface(service)
+            ?.takeIf { isInAppBillingSupported(it) }
+            ?.also { billingService = it }
+            ?: run {
+                TODO("In app billing isn't supported")
+            }
+    }
+
+    private fun isInAppBillingSupported(inAppBillingService: IInAppBillingService): Boolean {
+        val inAppBillingSupportState = inAppBillingService.isBillingSupported(
+            IN_APP_BILLING_VERSION,
+            context.packageName,
+            BILLING_ITEM_TYPE_IN_APP
+        )
+        return inAppBillingSupportState == SERVICE_RESPONSE_RESULT_OK
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {
+        billingService = null
+    }
+
+    companion object {
+        private const val SERVICE_RESPONSE_RESULT_OK = 0
+        private const val IN_APP_BILLING_VERSION = 3
+        private const val BILLING_SERVICE_ACTION = "ir.cafebazaar.pardakht.InAppBillingService.BIND"
+        private const val BAZAAR_PACKAGE_NAME = "com.farsitel.bazaar"
+        private const val BILLING_ITEM_TYPE_IN_APP = "inapp"
+    }
+
+}
