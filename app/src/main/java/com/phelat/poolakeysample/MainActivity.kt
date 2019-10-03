d52b510688723f8010ee6956f880e74e2470b2e7
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-10-03 23:55:22 +0330

    Initialize payment inside 'onCreate' function in MainActivity

--- app/src/main/java/com/phelat/poolakeysample/MainActivity.kt
+++ app/src/main/java/com/phelat/poolakeysample/MainActivity.kt
@@ -2,12 +2,18 @@ package com.phelat.poolakeysample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.phelat.poolakey.Payment
 
 class MainActivity : AppCompatActivity() {
+
+    private val payment by lazy(LazyThreadSafetyMode.NONE) {
+        Payment(context = this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        payment.initialize {}
     }
 
 }
