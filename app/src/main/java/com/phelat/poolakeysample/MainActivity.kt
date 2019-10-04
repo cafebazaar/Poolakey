401590af827b2ea76a0f7815d44da5f020ef3596
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-10-04 19:02:09 +0330

    Purchase item when user clicks on 'purchaseButton' in MainActivity

--- app/src/main/java/com/phelat/poolakeysample/MainActivity.kt
+++ app/src/main/java/com/phelat/poolakeysample/MainActivity.kt
@@ -3,7 +3,9 @@ package com.phelat.poolakeysample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.phelat.poolakey.Connection
+import com.phelat.poolakey.ConnectionState
 import com.phelat.poolakey.Payment
+import com.phelat.poolakey.PurchaseRequest
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,11 +30,26 @@ class MainActivity : AppCompatActivity() {
                 serviceConnectionStatus.setText(R.string.general_service_connection_not_connected_text)
             }
         }
+        purchaseButton.setOnClickListener {
+            if (paymentConnection.getState() == ConnectionState.Connected) {
+                payment.purchaseItem(
+                    activity = this,
+                    request = PurchaseRequest(
+                        sku = skuValueInput.text.toString(),
+                        requestCode = PURCHASE_REQUEST_CODE
+                    )
+                )
+            }
+        }
     }
 
     override fun onDestroy() {
         paymentConnection.disconnect()
         super.onDestroy()
+    }
+
+    companion object {
+        private const val PURCHASE_REQUEST_CODE = 1000
     }
 
 }
