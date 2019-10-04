a42f9a23d5278a35a3b5a651e6d8792c2b56254c
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-10-04 23:22:51 +0330

    Implement ResultParser class to parse received intent from Bazaar

--- poolakey/src/main/java/com/phelat/poolakey/BazaarIntent.kt
+++ poolakey/src/main/java/com/phelat/poolakey/BazaarIntent.kt
@@ -4,5 +4,7 @@ internal object BazaarIntent {
 
     const val RESPONSE_CODE = "RESPONSE_CODE"
     const val RESPONSE_RESULT_OK = 0
+    const val RESPONSE_PURCHASE_DATA = "INAPP_PURCHASE_DATA"
+    const val RESPONSE_SIGNATURE_DATA = "INAPP_DATA_SIGNATURE"
 
 }
---
+++ poolakey/src/main/java/com/phelat/poolakey/ResultParser.kt
@@ -0,0 +1,47 @@
+package com.phelat.poolakey
+
+import android.content.Intent
+import org.json.JSONObject
+
+internal class ResultParser {
+
+    private var callback: PurchaseCallback? = null
+
+    fun parseResult(data: Intent?, purchaseCallback: PurchaseCallback.() -> Unit) {
+        callback = PurchaseCallback().apply(purchaseCallback)
+        if (data?.extras?.get(BazaarIntent.RESPONSE_CODE) == BazaarIntent.RESPONSE_RESULT_OK) {
+            val purchaseData = data.getStringExtra(BazaarIntent.RESPONSE_PURCHASE_DATA)
+            val dataSignature = data.getStringExtra(BazaarIntent.RESPONSE_SIGNATURE_DATA)
+            if (purchaseData != null && dataSignature != null) {
+                JSONObject(purchaseData).run {
+                    PurchaseInfo(
+                        orderId = optString(ORDER_ID),
+                        purchaseToken = optString(PURCHASE_TOKEN),
+                        payload = optString(DEVELOPER_PAYLOAD),
+                        packageName = optString(PACKAGE_NAME),
+                        purchaseState = optInt(PURCHASE_STATE),
+                        purchaseTime = optLong(PURCHASE_TIME),
+                        productId = optString(PRODUCT_ID)
+                    )
+                }.also { purchaseInfo ->
+                    callback?.purchaseSucceed?.invoke(purchaseInfo)
+                }
+            } else {
+                callback?.purchaseFailed?.invoke()
+            }
+        } else {
+            callback?.purchaseFailed?.invoke()
+        }
+    }
+
+    companion object {
+        private const val ORDER_ID: String = "orderId"
+        private const val PURCHASE_TOKEN: String = "purchaseToken"
+        private const val DEVELOPER_PAYLOAD: String = "developerPayload"
+        private const val PACKAGE_NAME: String = "packageName"
+        private const val PURCHASE_STATE: String = "purchaseState"
+        private const val PURCHASE_TIME: String = "purchaseTime"
+        private const val PRODUCT_ID: String = "productId"
+    }
+
+}
