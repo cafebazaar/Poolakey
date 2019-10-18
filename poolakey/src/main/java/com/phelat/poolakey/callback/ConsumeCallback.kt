d08b423cd21c3549eeeff2d884bbb0dd59ba8eb2
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-10-18 22:21:02 +0330

    Rename 'exception' argument to 'throwable' in 'consumeFailed' function

--- poolakey/src/main/java/com/phelat/poolakey/callback/ConsumeCallback.kt
+++ poolakey/src/main/java/com/phelat/poolakey/callback/ConsumeCallback.kt
@@ -4,13 +4,13 @@ class ConsumeCallback {
 
     internal var consumeSucceed: () -> Unit = {}
 
-    internal var consumeFailed: (exception: Throwable) -> Unit = {}
+    internal var consumeFailed: (throwable: Throwable) -> Unit = {}
 
     fun consumeSucceed(block: () -> Unit) {
         consumeSucceed = block
     }
 
-    fun consumeFailed(block: (exception: Throwable) -> Unit) {
+    fun consumeFailed(block: (throwable: Throwable) -> Unit) {
         consumeFailed = block
     }
 
