e8ab2f2f5fab4850ef9626579fb286cf189d5380
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-11-02 11:09:51 +0330

    Implement a function in PurchaseInfo to convert it to JSONObject

--- poolakey/src/main/java/com/phelat/poolakey/entity/PurchaseInfo.kt
+++ poolakey/src/main/java/com/phelat/poolakey/entity/PurchaseInfo.kt
@@ -1,5 +1,8 @@
 package com.phelat.poolakey.entity
 
+import com.phelat.poolakey.constant.RawJson
+import org.json.JSONObject
+
 data class PurchaseInfo(
     val orderId: String,
     val purchaseToken: String,
@@ -8,4 +11,16 @@ data class PurchaseInfo(
     val purchaseState: PurchaseState,
     val purchaseTime: Long,
     val productId: String
-)
+) {
+
+    fun toJson() = with(JSONObject()) {
+        put(RawJson.ORDER_ID, orderId)
+        put(RawJson.PURCHASE_TOKEN, purchaseToken)
+        put(RawJson.DEVELOPER_PAYLOAD, payload)
+        put(RawJson.PACKAGE_NAME, packageName)
+        put(RawJson.PURCHASE_STATE, purchaseState.ordinal)
+        put(RawJson.PURCHASE_TIME, purchaseTime)
+        put(RawJson.PRODUCT_ID, productId)
+    }
+
+}
