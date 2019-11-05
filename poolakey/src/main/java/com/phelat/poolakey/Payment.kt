a88d4426efa593dbafb774356813cbeb36598390
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-11-05 20:11:35 +0330

    CleanUp documentation for 'getPurchasedItems' function in Payment

--- poolakey/src/main/java/com/phelat/poolakey/Payment.kt
+++ poolakey/src/main/java/com/phelat/poolakey/Payment.kt
@@ -150,11 +150,11 @@ class Payment(context: Context, config: PaymentConfiguration = PaymentConfigurat
 
     /**
      * You can use this function to query user's purchased products, Note that if you want to query
-     * user's subscribed products, you have to use 'getSubscribedItems' function since this function
+     * user's subscribed products, you have to use 'getSubscribedItems' function, since this function
      * will only query purchased products and not the subscribed products. This function runs off
      * the main thread, so you don't have to handle the threading by your self.
      * @see getSubscribedItems
-     * @param callback That's how you can get notified about query's result.
+     * @param callback You have to use callback in order to get notified about query's result.
      */
     fun getPurchasedItems(callback: PurchaseQueryCallback.() -> Unit) {
         connection.queryBoughtItems(PurchaseType.IN_APP, callback)
