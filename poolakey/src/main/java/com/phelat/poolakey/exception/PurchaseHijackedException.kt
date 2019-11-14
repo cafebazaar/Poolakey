49161f997cf1db94298c83ead9cd4a478d56f7af
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-11-14 18:37:35 +0330

    Implement PurchaseHijackedException

---
+++ poolakey/src/main/java/com/phelat/poolakey/exception/PurchaseHijackedException.kt
@@ -0,0 +1,10 @@
+package com.phelat.poolakey.exception
+
+import java.lang.Exception
+
+class PurchaseHijackedException : Exception() {
+
+    override val message: String?
+        get() = "The purchase was hijacked and it's not a valid purchase"
+
+}
