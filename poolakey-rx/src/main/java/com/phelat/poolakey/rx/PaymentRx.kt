2878a9da1135c4aff5c9ed79801d54f7289a4402
Davud <work.hosseinidavid@gmail.com>
2020-01-11 16:16:30 +0330

    Disconnect Connection on dispose

--- poolakey-rx/src/main/java/com/phelat/poolakey/rx/PaymentRx.kt
+++ poolakey-rx/src/main/java/com/phelat/poolakey/rx/PaymentRx.kt
@@ -22,12 +22,16 @@ import io.reactivex.Single
  * @return Observable that you can subscribe to it and get notified about service connection changes.
  */
 fun Payment.connect(): Observable<Connection> {
+    var connection: Connection? = null
     return Observable.create<Connection> { emitter ->
         connect {
+            connection = this
             connectionSucceed { emitter.onNext(this) }
             disconnected { emitter.onNext(this) }
             connectionFailed { emitter.onError(it) }
         }
+    }.doOnDispose {
+        connection?.disconnect()
     }
 }
 
