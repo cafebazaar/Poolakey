ebc1e604ec14fbc2c2170586e35441684972f882
M. Reza Nasirloo <m.reza@cafebazaar.ir>
2020-05-10 16:53:13 +0430

    Add originalJson to PurchaseInfo.kt instead of building it

--- poolakey/src/main/java/com/phelat/poolakey/entity/PurchaseInfo.kt
+++ poolakey/src/main/java/com/phelat/poolakey/entity/PurchaseInfo.kt
@@ -1,8 +1,5 @@
 package com.phelat.poolakey.entity
 
-import com.phelat.poolakey.constant.RawJson
-import org.json.JSONObject
-
 data class PurchaseInfo(
     val orderId: String,
     val purchaseToken: String,
@@ -11,17 +8,6 @@ data class PurchaseInfo(
     val purchaseState: PurchaseState,
     val purchaseTime: Long,
     val productId: String,
+    val originalJson: String,
     val dataSignature: String
-) {
-
-    fun toJson() = with(JSONObject()) {
-        put(RawJson.ORDER_ID, orderId)
-        put(RawJson.PURCHASE_TOKEN, purchaseToken)
-        put(RawJson.DEVELOPER_PAYLOAD, payload)
-        put(RawJson.PACKAGE_NAME, packageName)
-        put(RawJson.PURCHASE_STATE, purchaseState.ordinal)
-        put(RawJson.PURCHASE_TIME, purchaseTime)
-        put(RawJson.PRODUCT_ID, productId)
-    }
-
-}
+)
