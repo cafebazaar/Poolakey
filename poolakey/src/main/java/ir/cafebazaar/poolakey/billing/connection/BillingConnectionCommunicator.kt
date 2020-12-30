45f0a08f7b23e8b9d7cd8e027ff593ccec60a639
Shayan <shayanpourvatan@gmail.com>
2020-12-30 19:42:09 +0330

    Create BillingConnectionCommunicator as component communicator

---
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/billing/connection/BillingConnectionCommunicator.kt
@@ -0,0 +1,45 @@
+package ir.cafebazaar.poolakey.billing.connection
+
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import ir.cafebazaar.poolakey.PurchaseType
+import ir.cafebazaar.poolakey.callback.ConnectionCallback
+import ir.cafebazaar.poolakey.callback.ConsumeCallback
+import ir.cafebazaar.poolakey.callback.PurchaseIntentCallback
+import ir.cafebazaar.poolakey.callback.PurchaseQueryCallback
+import ir.cafebazaar.poolakey.request.PurchaseRequest
+
+internal interface BillingConnectionCommunicator {
+
+    fun startConnection(
+        context: Context,
+        callback: ConnectionCallback
+    ): Boolean
+
+    fun isPurchaseTypeSupported(
+        purchaseType: PurchaseType
+    ): Boolean
+
+    fun consume(
+        purchaseToken: String,
+        callback: ConsumeCallback.() -> Unit
+    )
+
+    fun queryPurchasedProducts(
+        purchaseType: PurchaseType,
+        callback: PurchaseQueryCallback.() -> Unit
+    )
+
+    fun purchase(
+        purchaseRequest: PurchaseRequest,
+        purchaseType: PurchaseType,
+        callback: PurchaseIntentCallback.() -> Unit,
+        fireIntentSender: (IntentSender) -> Unit,
+        fireIntent: (Intent) -> Unit
+    )
+
+    fun stopConnection()
+
+    fun disconnect()
+}
