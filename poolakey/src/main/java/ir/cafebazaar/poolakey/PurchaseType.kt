98bd71bd413a9268af4820ff72e9a9be0e971395
Shayan <shayanpourvatan@gmail.com>
2021-04-20 13:48:29 +0430

    Define fromValue method in PurchaseType to convert string to PurchaseType

--- poolakey/src/main/java/ir/cafebazaar/poolakey/PurchaseType.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/PurchaseType.kt
@@ -2,5 +2,12 @@ package ir.cafebazaar.poolakey
 
 internal enum class PurchaseType(val type: String) {
     IN_APP("inapp"),
-    SUBSCRIPTION("subs")
+    SUBSCRIPTION("subs");
+
+    companion object {
+        internal fun fromValue(type: String): PurchaseType {
+            return values().firstOrNull { it.type == type }
+                ?: throw IllegalArgumentException("invalid purchase type")
+        }
+    }
 }
