694ea0f77322b547e56ce7663ffb304e08388c58
Shayan <shayanpourvatan@gmail.com>
2021-04-20 14:33:22 +0430

    Remove redundant fromValue method from PurchaseType

--- poolakey/src/main/java/ir/cafebazaar/poolakey/PurchaseType.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/PurchaseType.kt
@@ -2,12 +2,5 @@ package ir.cafebazaar.poolakey
 
 enum class PurchaseType(val type: String) {
     IN_APP("inapp"),
-    SUBSCRIPTION("subs");
-
-    companion object {
-        internal fun fromValue(type: String): PurchaseType {
-            return values().firstOrNull { it.type == type }
-                ?: throw IllegalArgumentException("invalid purchase type")
-        }
-    }
+    SUBSCRIPTION("subs")
 }
