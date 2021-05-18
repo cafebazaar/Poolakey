65acfde9e307726888c0524ef9d18a0af5b98559
Shayan <shayanpourvatan@gmail.com>
2021-05-18 15:50:16 +0430

    Handle intentV3 in PurchaseFunction

--- poolakey/src/main/java/ir/cafebazaar/poolakey/billing/purchase/PurchaseFunction.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/billing/purchase/PurchaseFunction.kt
@@ -11,8 +11,10 @@ import ir.cafebazaar.poolakey.billing.BillingFunction
 import ir.cafebazaar.poolakey.callback.PurchaseIntentCallback
 import ir.cafebazaar.poolakey.constant.BazaarIntent
 import ir.cafebazaar.poolakey.constant.Billing
+import ir.cafebazaar.poolakey.exception.BazaarNotSupportedException
 import ir.cafebazaar.poolakey.exception.ResultNotOkayException
 import ir.cafebazaar.poolakey.request.PurchaseRequest
+import ir.cafebazaar.poolakey.request.purchaseExtraData
 import ir.cafebazaar.poolakey.takeIf
 
 internal class PurchaseFunction(
@@ -34,35 +36,81 @@ internal class PurchaseFunction(
                     .invoke(IllegalStateException("Couldn't receive buy intent from Bazaar"))
             }
 
-            if (doesClientSupportIntentV2(purchaseConfigBundle)) {
-                getBuyIntentV2FromBillingService(
-                    billingService,
-                    purchaseRequest,
-                    purchaseType,
-                    callback
-                )?.takeIf(
-                    thisIsTrue = { bundle ->
-                        bundle.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY) != null
-                    }, andIfNot = intentResponseIsNullError
-                )?.getParcelable<Intent>(INTENT_RESPONSE_BUY)?.also { purchaseIntent ->
-                    fireIntentWithIntent.invoke(purchaseIntent)
+            when {
+                doesClientSupportIntentV3(purchaseConfigBundle) -> {
+                    fireBuyIntentV3(billingService, intentResponseIsNullError)
                 }
-            } else {
-                getBuyIntentFromBillingService(
-                    billingService,
-                    purchaseRequest,
-                    purchaseType,
-                    callback
-                )?.takeIf(
-                    thisIsTrue = { bundle ->
-                        bundle.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY) != null
-                    }, andIfNot = intentResponseIsNullError
-                )?.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY)?.also { purchaseIntent ->
-                    fireIntentWithIntentSender.invoke(purchaseIntent.intentSender)
+                doesClientSupportIntentV2(purchaseConfigBundle) -> {
+                    fireBuyIntentV2(billingService, intentResponseIsNullError)
+                }
+                else -> {
+                    fireBuyIntent(billingService, intentResponseIsNullError)
                 }
             }
         } catch (e: RemoteException) {
             PurchaseIntentCallback().apply(callback).failedToBeginFlow.invoke(e)
+        }
+    }
+
+    private fun PurchaseFunctionRequest.fireBuyIntentV3(
+        billingService: IInAppBillingService,
+        intentResponseIsNullError: () -> Unit
+    ) {
+
+        if (purchaseRequest.discount.isNullOrEmpty().not()) {
+            PurchaseIntentCallback().apply(callback).failedToBeginFlow.invoke(
+                BazaarNotSupportedException()
+            )
+            return
+        }
+
+        getBuyIntentV3FromBillingService(
+            billingService,
+            purchaseRequest,
+            purchaseRequest.purchaseExtraData(),
+            callback
+        )?.takeIf(
+            thisIsTrue = { bundle ->
+                bundle.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY) != null
+            }, andIfNot = intentResponseIsNullError
+        )?.getParcelable<Intent>(INTENT_RESPONSE_BUY)?.also { purchaseIntent ->
+            fireIntentWithIntent.invoke(purchaseIntent)
+        }
+    }
+
+    private fun PurchaseFunctionRequest.fireBuyIntentV2(
+        billingService: IInAppBillingService,
+        intentResponseIsNullError: () -> Unit
+    ) {
+        getBuyIntentV2FromBillingService(
+            billingService,
+            purchaseRequest,
+            purchaseType,
+            callback
+        )?.takeIf(
+            thisIsTrue = { bundle ->
+                bundle.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY) != null
+            }, andIfNot = intentResponseIsNullError
+        )?.getParcelable<Intent>(INTENT_RESPONSE_BUY)?.also { purchaseIntent ->
+            fireIntentWithIntent.invoke(purchaseIntent)
+        }
+    }
+
+    private fun PurchaseFunctionRequest.fireBuyIntent(
+        billingService: IInAppBillingService,
+        intentResponseIsNullError: () -> Unit
+    ) {
+        getBuyIntentFromBillingService(
+            billingService,
+            purchaseRequest,
+            purchaseType,
+            callback
+        )?.takeIf(
+            thisIsTrue = { bundle ->
+                bundle.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY) != null
+            }, andIfNot = intentResponseIsNullError
+        )?.getParcelable<PendingIntent>(INTENT_RESPONSE_BUY)?.also { purchaseIntent ->
+            fireIntentWithIntentSender.invoke(purchaseIntent.intentSender)
         }
     }
 
@@ -92,15 +140,28 @@ internal class PurchaseFunction(
         purchaseRequest.payload
     )?.takeBundleIfResponseIsOk(callback)
 
+    private inline fun getBuyIntentV3FromBillingService(
+        billingService: IInAppBillingService,
+        purchaseRequest: PurchaseRequest,
+        extraData: Bundle,
+        callback: PurchaseIntentCallback.() -> Unit
+    ) = billingService.getBuyIntentV3(
+        Billing.IN_APP_BILLING_VERSION,
+        context.packageName,
+        purchaseRequest.productId,
+        purchaseRequest.payload,
+        extraData
+    )?.takeBundleIfResponseIsOk(callback)
+
     private inline fun Bundle.takeBundleIfResponseIsOk(
         callback: PurchaseIntentCallback.() -> Unit
     ): Bundle? = takeIf(
         thisIsTrue = { bundle ->
             bundle.get(BazaarIntent.RESPONSE_CODE) == BazaarIntent.RESPONSE_RESULT_OK
         }, andIfNot = {
-        PurchaseIntentCallback().apply(callback)
-            .failedToBeginFlow
-            .invoke(ResultNotOkayException())
+            PurchaseIntentCallback().apply(callback)
+                .failedToBeginFlow
+                .invoke(ResultNotOkayException())
         }
     )
 
@@ -108,9 +169,14 @@ internal class PurchaseFunction(
         return purchaseConfigBundle?.getBoolean(INTENT_V2_SUPPORT) ?: false
     }
 
+    private fun doesClientSupportIntentV3(purchaseConfigBundle: Bundle?): Boolean {
+        return purchaseConfigBundle?.getBoolean(INTENT_V3_SUPPORT) ?: false
+    }
+
     companion object {
         private const val INTENT_RESPONSE_BUY = "BUY_INTENT"
         private const val INTENT_V2_SUPPORT = "INTENT_V2_SUPPORT"
+        private const val INTENT_V3_SUPPORT = "INTENT_V3_SUPPORT"
     }
 
 }
