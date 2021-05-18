70a2d7fda495e8f4bcc42e93bff7cb8f55c4d604
Shayan <shayanpourvatan@gmail.com>
2021-05-18 15:48:39 +0430

    Add developer discount in purchase request

--- poolakey/src/main/java/ir/cafebazaar/poolakey/request/PurchaseRequest.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/request/PurchaseRequest.kt
@@ -1,7 +1,17 @@
 package ir.cafebazaar.poolakey.request
 
+import android.os.Bundle
+import ir.cafebazaar.poolakey.constant.BazaarIntent
+
 data class PurchaseRequest(
     val productId: String,
     val requestCode: Int,
-    val payload: String? = null
+    val payload: String? = null,
+    val discount: String?
 )
+
+internal fun PurchaseRequest.purchaseExtraData(): Bundle {
+    return Bundle().apply {
+        putString(BazaarIntent.RESPONSE_DEVELOPER_DISCOUNT, discount)
+    }
+}
