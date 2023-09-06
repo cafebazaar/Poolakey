50152ef7f01026f169c450a6f88b0f61cc6c735a
Seyyed davud hosseiny <hosseinydavid@gmail.com>
2023-09-06 10:32:19 +0330

    Support displayCutOutMode Short edges

--- poolakey/src/main/java/ir/cafebazaar/poolakey/request/PurchaseRequest.kt
+++ poolakey/src/main/java/ir/cafebazaar/poolakey/request/PurchaseRequest.kt
@@ -7,10 +7,14 @@ data class PurchaseRequest(
     val productId: String,
     val payload: String? = null,
     val dynamicPriceToken: String? = null
-)
+) {
+
+    internal var cutoutModeIsShortEdges = false
+}
 
 internal fun PurchaseRequest.purchaseExtraData(): Bundle {
     return Bundle().apply {
         putString(BazaarIntent.RESPONSE_DYNAMIC_PRICE_TOKEN, dynamicPriceToken)
+        putBoolean(BazaarIntent.RESPONSE_CUTOUT_MODE_IS_SHORT_EDGES, cutoutModeIsShortEdges)
     }
 }
